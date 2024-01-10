e32f71c849e516aef129f17cc840a732ed1e0394
Chancellor <me@chancellor.tech>
2024-01-10 02:59:46 -0600

    change color scheme to mellow

--- after/plugin/.colorscheme.lua
+++ after/plugin/.colorscheme.lua
@@ -1,6 +1,7 @@
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme mellow]]
 
 vim.keymap.set('n', '<leader>1', vim.cmd.Inspect, { desc = "Inspect current highlight item" })
 vim.keymap.set('n', '<leader>2', vim.cmd.TSPlaygroundToggle, { desc = "Inspect current highlight item" })
 vim.keymap.set('n', '<leader>4', '<cmd>:colorscheme shopkeep<CR>', { desc = "Colorscheme change to shopkeep" })
 vim.keymap.set('n', '<leader>5', '<cmd>:colorscheme tokyonight<CR>', { desc = "Colorscheme change to tokyonight" })
+vim.keymap.set('n', '<leader>6', '<cmd>:colorscheme mellow<CR>', { desc = "Colorscheme change to mellow" })
